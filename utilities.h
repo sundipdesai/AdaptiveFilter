1a2f9f323378911dd48244c7fddabcd9afdb784b
sundipdesai <sundipdesai@gmail.com>
2015-08-15 09:46:40 -0700

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,18 @@
+// Math utilities, constants and functions
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+//*********** CONSTANTS *************//
+const int filterOrder = 25;  // filter order 
+const double beta = 0.0007;		//learning rate */
+const int numSamples = 160000;
+//*********** CONSTANTS *************//
+
+
+
+
+
+
+
+
+#endif
